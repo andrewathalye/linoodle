d546c613220651af2cc653c4abc77af75ddc0c52
Andrew Athalye <andrewathalye@outlook.com>
2022-06-20 17:28:42 +0000

    Add C-style error checking. TODO - make more C++-friendly.

--- linoodle.cpp
+++ linoodle.cpp
@@ -1,44 +1,42 @@
 
 #include "windows_library.h"
+#include <iostream>
 
 typedef __attribute__((ms_abi)) size_t(*tDecompressFunc)(uint8_t * srcBuf, size_t srcLen, uint8_t * dstBuf, size_t dstLen, int64_t fuzz, int64_t crc, int64_t verbose, uint8_t * dstBase, size_t e, void * cb, void * cbCtx, void * scratch, size_t scratchSize, int64_t threadPhase);
 
 typedef __attribute__((ms_abi)) size_t(*tCompressFunc)(int64_t codec, uint8_t * srcBuf, size_t srcLen, uint8_t * dstBuf, int64_t level, void * opts, size_t offs, size_t unused, void * scratch, size_t scratchSize);
 
-class OodleWrapper {
-public:
-    OodleWrapper() :
-        m_oodleLib(WindowsLibrary::Load("oo2core_3_win64.dll"))
-    {
-        m_decompressFunc = reinterpret_cast<tDecompressFunc>(m_oodleLib.GetExport("OodleLZ_Decompress"));
-        m_compressFunc = reinterpret_cast<tCompressFunc>(m_oodleLib.GetExport("OodleLZ_Compress"));
-    }
+WindowsLibrary* wl = NULL;
+tDecompressFunc m_decompressFunc;
+tCompressFunc m_compressFunc;
 
-    size_t Decompress(uint8_t * srcBuf, size_t srcLen, uint8_t * dstBuf, size_t dstLen, int64_t fuzz, int64_t crc, int64_t verbose, uint8_t * dstBase, size_t e, void * cb, void * cbCtx, void * scratch, size_t scratchSize, int64_t threadPhase)
-    {
-        WindowsLibrary::SetupCall();
-        return m_decompressFunc(srcBuf, srcLen, dstBuf, dstLen, fuzz, crc, verbose, dstBase, e, cb, cbCtx, scratch, scratchSize, threadPhase);
+void initWrapper (void) {
+    try {
+    	wl = new WindowsLibrary(WindowsLibrary::Load("oo2core_3_win64.dll"));
+    	m_decompressFunc = reinterpret_cast<tDecompressFunc>(wl->GetExport("OodleLZ_Decompress"));
+    	m_compressFunc = reinterpret_cast<tCompressFunc>(wl->GetExport("OodleLZ_Compress"));
+    } catch (std::exception &e) {
+    	std::cerr << "[Error] Could not initialise Oodle library: " << e.what() << "\n";
+	exit(-1);
     }
-    size_t Compress(int64_t codec, uint8_t * srcBuf, size_t srcLen, uint8_t * dstBuf, int64_t level, void * opts, size_t offs, size_t unused, void * scratch, size_t scratchSize)
-    {
-        WindowsLibrary::SetupCall();
-        return m_compressFunc(codec, srcBuf, srcLen, dstBuf, level, opts, offs, unused, scratch, scratchSize);
-    }
-
-private:
-    WindowsLibrary m_oodleLib;
-    tDecompressFunc m_decompressFunc;
-    tCompressFunc m_compressFunc;
-};
-
-OodleWrapper g_oodleWrapper;
+}
 
 extern "C" size_t LinoodleLZ_Decompress(uint8_t * srcBuf, size_t srcLen, uint8_t * dstBuf, size_t dstLen, int64_t fuzz, int64_t crc, int64_t verbose, uint8_t * dstBase, size_t e, void * cb, void * cbCtx, void * scratch, size_t scratchSize, int64_t threadPhase)
 {
-    return g_oodleWrapper.Decompress(srcBuf, srcLen, dstBuf, dstLen, fuzz, crc, verbose, dstBase, e, cb, cbCtx, scratch, scratchSize, threadPhase);
+	if (!wl) {
+		initWrapper();
+	}
+
+	WindowsLibrary::SetupCall();
+	return m_decompressFunc(srcBuf, srcLen, dstBuf, dstLen, fuzz, crc, verbose, dstBase, e, cb, cbCtx, scratch, scratchSize, threadPhase);
 }
 
 extern "C" size_t LinoodleLZ_Compress(int64_t codec, uint8_t * srcBuf, size_t srcLen, uint8_t * dstBuf, int64_t level, void * opts, size_t offs, size_t unused, void * scratch, size_t scratchSize)
 {
-    return g_oodleWrapper.Compress(codec, srcBuf, srcLen, dstBuf, level, opts, offs, unused, scratch, scratchSize);
+	if (!wl) {
+		initWrapper();
+	}
+
+	WindowsLibrary::SetupCall();
+	return m_compressFunc(codec, srcBuf, srcLen, dstBuf, level, opts, offs, unused, scratch, scratchSize);
 }
